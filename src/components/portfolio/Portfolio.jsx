609a6c7219bf21e378b91c94cdc61544c75867d8
Abhishek Kumar <abhishekkumar@Abhisheks-MacBook-Air.local>
2025-08-20 12:36:06 +0530

    Added new project

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,20 +7,30 @@ import IMG4 from "../../assets/portfolio4.jpg";
 import IMG5 from "../../assets/portfolio5.jpg";
 import IMG6 from "../../assets/portfolio6.jpg";
 import IMG7 from "../../assets/portfolio7.jpg";
-import IMG8 from "../../assets/portfolio8.jpg";
+// import IMG8 from "../../assets/portfolio8.jpg";
 import IMG9 from "../../assets/portfolio9.jpg";
 import IMG10 from "../../assets/portfolio10.jpg";
 import IMG11 from "../../assets/portfolio11.jpg";
 import IMG12 from "../../assets/portfolio12.jpg";
+import IMG13 from "../../assets/portfolio13.jpg";
 
 const data = [
   {
-    id: 8,
-    image: IMG8,
-    title: "My Portfolio",
-    github: "https://github.com/kumarabhishek188/My-Portfolio",
-    demo: "https://abhishek-portfolio2025.netlify.app/",
+    id: 13,
+    image: IMG13,
+    title: "Abhishek Medical - My Own Medicine Shop Website",
+    // github: "https://github.com/kumarabhishek188/Medical",
+    demo: "https://abhishekmedicaladapur.netlify.app/",
   },
+
+  //   {
+  //   id: 8,
+  //   image: IMG8,
+  //   title: "My Portfolio",
+  //   github: "https://github.com/kumarabhishek188/My-Portfolio",
+  //   demo: "https://abhishek-portfolio2025.netlify.app/",
+  // },
+  
 
   {
     id: 7,
